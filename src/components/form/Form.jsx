5fb17dfe5e2838dfe133e9eb5caf3de5f0c79b69
Mina Yoo <maydec2298@gmail.com>
2022-08-13 21:18:15 +0900

    first commit

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./style.css"
 
 const Form = () => {
+  const [title, setTitle] = useState("")
+
+  const onChangeHandler = (event) => {
+    const inputTitle = event.target.title;
+    setTitle(inputTitle);
+  };
+  console.log(title)
 
   return (
     <div className='form_container'>
 
-      <div>제목<input type="text" className="form_input" /></div>
+      <div>제목<input type="text" className="form_input" onChange={onChangeHandler} value={title} /></div>
       <div>내용<input type="text" className="form_input" /></div>
-      <div><button className="button">추가하기</button></div>
+      <div><button className="button">추가하기추가추가</button></div>
 
     </div>
   )
